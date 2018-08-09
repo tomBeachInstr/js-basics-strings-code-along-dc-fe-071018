c9d8315188087e37c6ec0418126a6a6a5a7da371
Thomas E Beach <thomas.beach@flatironschool.com>
2018-08-09 23:06:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 const currentUser = 'Grace Hopper';
 const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const welcomeMsg1 = 'Welcome to Flatbook, ';
+const welcomeMsg2 = welcomeMsg1.toUpperCase;
+console.log("welcomeMsg2:", welcomeMsg2)
